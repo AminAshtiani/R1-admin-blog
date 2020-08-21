b3dfa9a05fb77d011b1f572f27f2e65d1bfa4762
Amin <a.mohebi@nak-mci.ir>
2020-08-21 14:50:44 +0430

    adding bootstrap to project
    install bootstrap
    register bootstrap
    create a scss file and import sass file
    test a button in about page

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import './custom.scss';
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
   router,
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <b-button variant="danger">Button</b-button>
   </div>
 </template>
+<script>
+// @ is an alias to /src
+import { BButton } from 'bootstrap-vue'
+export default {
+  name: 'About',
+  components: {
+    'b-button': BButton
+  }
+}
+</script>
